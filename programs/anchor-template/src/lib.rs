3916b886ad7c385953a72b2285dccc9e8a5e50f1
Piero Rolando <rolandopiero63@gmail.com>
2022-02-03 15:42:33 -0500

    works

--- programs/anchor-template/src/lib.rs
+++ programs/anchor-template/src/lib.rs
@@ -1,14 +1,39 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("HBkEJDx3aSQVVztJf86zkwkG44B9RdU8sd44xvR7rNjc");
 
 #[program]
 pub mod anchor_template {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+    pub fn initialize(ctx: Context<InitializeCounter>) -> ProgramResult {
+        let counter_account = &mut ctx.accounts.counter_account;
+        counter_account.counter = 0;
+        Ok(())
+    }
+
+    pub fn add(ctx: Context<AddCounter>) -> ProgramResult {
+        let counter_account = &mut ctx.accounts.counter_account;
+        counter_account.counter += 1;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitializeCounter<'info> {
+    #[account(init, payer = user, space = 9000)]
+    pub counter_account: Account<'info, CounterAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct AddCounter<'info> {
+    #[account(mut)]
+    pub counter_account: Account<'info, CounterAccount>,
+}
+
+#[account]
+pub struct CounterAccount {
+    pub counter: u64,
+}
--- tests/anchor-template.js
+++ tests/anchor-template.js
@@ -2,13 +2,65 @@ const anchor = require('@project-serum/anchor');
 
 describe('anchor-template', () => {
 
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
+
+  const program = anchor.workspace.AnchorTemplate;
+
+  const counterAccount = anchor.web3.Keypair.generate()
 
   it('Is initialized!', async () => {
     // Add your test here.
-    const program = anchor.workspace.AnchorTemplate;
-    const tx = await program.rpc.initialize();
+
+    console.log("HOLAAAAAA", program.programId)
+    const tx = await program.rpc.initialize({
+      accounts: {
+        counterAccount: counterAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId
+      },
+      signers: [counterAccount]
+    });
     console.log("Your transaction signature", tx);
   });
+
+  it("Add +1", async () => {
+
+    let account = await program.account.counterAccount.fetch(counterAccount.publicKey);
+    console.log('👀Count', account.counter.toString())
+
+    // Call add_gif!
+    await program.rpc.add({
+      accounts: {
+        counterAccount: counterAccount.publicKey,
+      },
+    });
+
+    account = await program.account.counterAccount.fetch(counterAccount.publicKey);
+    console.log('👀Count', account.counter.toString())
+
+  })
 });
+
+describe("Vector", () => {
+  const provider = anchor.Provider.env()
+  anchor.setProvider(provider)
+
+  const program = anchor.workspace.Likearray
+
+  const itemsAccount = anchor.web3.Keypair.generate()
+
+  it("Is init", async () => {
+    const tx = await program.rpc.initialize({
+      accounts: {
+        arrayAccount: itemsAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId
+      },
+      signers: [itemsAccount]
+    })
+
+    console.log("Your other signature", tx)
+  })
+})
